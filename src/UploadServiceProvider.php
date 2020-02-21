8b9c1a5c0cd433ab81a98211af1f65d5e194ea89
tangqi <tangqi_1987@sina.com>
2020-02-21 15:47:40 +0800

    添加路由命名

--- src/UploadServiceProvider.php
+++ src/UploadServiceProvider.php
@@ -10,45 +10,45 @@ use Route;
 
 class UploadServiceProvider extends ServiceProvider
 {
-	protected $namespace = 'iBrand\Upload';
+    protected $namespace = 'iBrand\Upload';
 
-	public function boot()
-	{
-		if ($this->app->runningInConsole()) {
-			$this->publishes([__DIR__ . '/../config/config.php' => config_path('ibrand/uploader.php')], 'config');
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([__DIR__ . '/../config/config.php' => config_path('ibrand/uploader.php')], 'config');
 
-			$this->publishes([__DIR__ . '/../migrations/' => database_path('migrations')], 'migrations');
-		}
+            $this->publishes([__DIR__ . '/../migrations/' => database_path('migrations')], 'migrations');
+        }
 
-		$this->registerMigrations();
+        $this->registerMigrations();
 
-		$this->map();
-	}
+        $this->map();
+    }
 
-	public function register()
-	{
-		$this->mergeConfigFrom(
-			__DIR__ . '/../config/config.php', 'ibrand.uploader'
-		);
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config/config.php', 'ibrand.uploader'
+        );
 
-		$filesystems = $this->app['config']->get('filesystems.disks', []);
+        $filesystems = $this->app['config']->get('filesystems.disks', []);
 
-		$this->app['config']->set('filesystems.disks', array_merge($filesystems, config('ibrand.uploader.disks')));
+        $this->app['config']->set('filesystems.disks', array_merge($filesystems, config('ibrand.uploader.disks')));
 
-		$this->app->alias(ImageChecker::class, ImageChecker::TYPE);
-		$this->app->alias(VideoChecker::class, VideoChecker::TYPE);
-		$this->app->alias(VoiceChecker::class, VoiceChecker::TYPE);
-	}
+        $this->app->alias(ImageChecker::class, ImageChecker::TYPE);
+        $this->app->alias(VideoChecker::class, VideoChecker::TYPE);
+        $this->app->alias(VoiceChecker::class, VoiceChecker::TYPE);
+    }
 
-	public function map()
-	{
-		Route::group(['namespace' => $this->namespace], function ($router) {
-			$router->post('cdn/upload', 'UploadController@upload');
-		});
-	}
+    public function map()
+    {
+        Route::group(['namespace' => $this->namespace], function ($router) {
+            $router->post('cdn/upload', 'UploadController@upload')->name('cdn.qiniu.upload');
+        });
+    }
 
-	public function registerMigrations()
-	{
-		return $this->loadMigrationsFrom(__DIR__ . '/../migrations');
-	}
+    public function registerMigrations()
+    {
+        return $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+    }
 }
