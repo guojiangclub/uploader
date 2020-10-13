1d9caec92132d4fd42fdc821d26258507105fc2e
tangqi <tangqi_1987@sina.com>
2020-10-13 15:06:38 +0800

    修改model 初始化数据

--- src/UploadFiles.php
+++ src/UploadFiles.php
@@ -10,10 +10,10 @@ class UploadFiles extends Model
 
 	public function __construct(array $attributes = [])
 	{
-		parent::__construct($attributes);
-
 		$prefix = config('ibrand.app.database.prefix', 'ibrand_');
 
 		$this->setTable($prefix . 'cdn_files');
+
+        parent::__construct($attributes);
 	}
 }
